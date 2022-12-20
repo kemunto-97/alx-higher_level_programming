bda64ffc3adaf15222fdb04a1824c68b8f88d12f
kemunto-97 <kemuntonyamweya@gmail.com>
2022-12-20 16:10:40 +0000

    typo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,16 +1,4 @@
-#!/usr/bin/python3
-
-"""
-
-Module 6-square
-
-Defines class Square with private size and position; and public area
-
-Can access and update size and position
-
-Can print to stdout the square using #'s
-
-"""
+"""Define a class Square."""
 
 
 
@@ -18,51 +6,19 @@ Can print to stdout the square using #'s
 
 class Square:
 
-    """
-
-    class Square definition
-
-
-
-    Args:
-
-        size (int): size of a side in square
-
-
-
-    Functions:
-
-        __init__(self, size, position)
-
-        size(self)
-
-        size(self, value)
-
-        position(self)
-
-        position(self, value)
-
-        area(self)
-
-        my_print(self)
-
-    """
+    """Represent a square."""
 
 
 
     def __init__(self, size=0, position=(0, 0)):
 
-        """
+        """Initialize a new square.
 
-        Initializes square
+        Args:
 
+            size (int): The size of the new square.
 
-
-        Attributes:
-
-            size (int): defaults to 0 if none; don't use __size to call setter
-
-            position (int): tuple of two positive integers
+            position (int, int): The position of the new square.
 
         """
 
@@ -76,17 +32,9 @@ class Square:
 
     def size(self):
 
-        """"
+        """Get/set the current size of the square."""
 
-        Getter
-
-
-
-        Return: size
-
-        """
-
-        return self.__size
+        return (self.__size)
 
 
 
@@ -94,19 +42,7 @@ class Square:
 
     def size(self, value):
 
-        """
-
-        Setter
-
-
-
-        Args:
-
-            value: sets size to value if int and >= 0
-
-        """
-
-        if type(value) is not int:
+        if not isinstance(value, int):
 
             raise TypeError("size must be an integer")
 
@@ -114,9 +50,7 @@ class Square:
 
             raise ValueError("size must be >= 0")
 
-        else:
-
-            self.__size = value
+        self.__size = value
 
 
 
@@ -124,17 +58,9 @@ class Square:
 
     def position(self):
 
-        """"
+        """Get/set the current position of the square."""
 
-        Getter
-
-
-
-        Return: position
-
-        """
-
-        return self.__position
+        return (self.__position)
 
 
 
@@ -142,62 +68,46 @@ class Square:
 
     def position(self, value):
 
-        """
+        if (not isinstance(value, tuple) or
 
-        Setter
+                len(value) != 2 or
 
+                not all(isinstance(num, int) for num in value) or
 
-
-        Args:
-
-            value: sets position to tuple if value is tuple of 2 positive ints
-
-        """
-
-        if type(value) is not tuple or len(value) != 2 or \
-
-           type(value[0]) is not int or type(value[1]) is not int or \
-
-           value[0] < 0 or value[1] < 0:
+                not all(num >= 0 for num in value)):
 
             raise TypeError("position must be a tuple of 2 positive integers")
 
-        else:
-
-            self.__position = value
+        self.__position = value
 
 
 
     def area(self):
 
-        """
+        """Return the current area of the square."""
 
-        Calculates area of square
-
-
-
-        Returns:
-
-            area
-
-        """
-
-        return (self.__size)**2
+        return (self.__size * self.__size)
 
 
 
     def my_print(self):
 
+        """Print the square with the # character."""
+
         if self.__size == 0:
 
             print("")
 
-        else:
+            return
 
-            print("\n" * self.__position[1], end="")
 
-            print("\n".join([" " * self.__position[0] +
 
-                             "#" * self.__size
+        [print("") for i in range(0, self.__position[1])]
 
-                             for rows in range(self.__size)]))
+        for i in range(0, self.__size):
+
+            [print(" ", end="") for j in range(0, self.__position[0])]
+
+            [print("#", end="") for k in range(0, self.__size)]
+
+            print("")
