f7feff5d717614fa9b7427283015ad0c7702dd6e
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-03-13 17:05:43 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-
-const numsArray = process.argv.slice(2);
-function secondMax (array) {
-  if (array.length < 2) {
-    return (0);
-  } else {
-    array.sort((x, y) => x - y);
-    array.pop();
-    return (array.pop());
-  }
-}
-console.log(secondMax(numsArray));
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
