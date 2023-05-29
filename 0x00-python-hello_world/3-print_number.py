548470e2308a1be205a88270bad8155ea817700b
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-05-29 11:33:37 +0300

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 number = 98
-try: print(f"{number} Battery street")
-except ValueError: print("ValueError: Unknown format code 'd' for object of type 'str'")
+print(f"{number} Battery street")
