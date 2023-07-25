47ab808c0b813b0d2f81f77bf721cad9e17ec48a
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-07-25 16:12:54 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.error('Usage: node script.js <file path> <string to write>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    console.log('Content written to the file successfully!');
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
