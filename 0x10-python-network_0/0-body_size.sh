a221fbafe5a5d830096d2615d7f29182d6945ae1
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-08-28 05:58:02 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
+# Get the size of the response header from a URL.
+curl -s "$1" | wc -c
+
